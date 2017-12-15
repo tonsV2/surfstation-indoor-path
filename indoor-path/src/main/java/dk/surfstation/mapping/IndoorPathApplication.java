5437d08b4180679713b73707a91c9288e9453419
ahj <ahj@altapay.com>
2017-12-15 12:12:27 -0200

    Initial commit

---
+++ indoor-path/src/main/java/dk/surfstation/mapping/IndoorPathApplication.java
@@ -0,0 +1,12 @@
+package dk.surfstation.mapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndoorPathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndoorPathApplication.class, args);
+	}
+}
