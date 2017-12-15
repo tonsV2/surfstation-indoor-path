91bb2341cc06b09c9e8cf898dcb6188a45618d1d
ahj <ahj@altapay.com>
2017-12-15 18:59:13 -0200

    Removed unused import

--- indoor-path/src/main/java/dk/surfstation/mapping/repository/EntranceRepository.java
+++ indoor-path/src/main/java/dk/surfstation/mapping/repository/EntranceRepository.java
@@ -2,7 +2,6 @@ package dk.surfstation.mapping.repository;
 
 import dk.surfstation.mapping.domain.Entrance;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface EntranceRepository extends JpaRepository<Entrance, Long> {
 }
